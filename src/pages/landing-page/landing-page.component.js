09c496c6f123135a5dd50abd16326b4abe294a65
landon-sturgeon <sturgeon.landon@gmail.com>
2020-01-07 07:08:27 -0800

    removed boilerplate files, added imgs and scss files

---
+++ src/pages/landing-page/landing-page.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { LandingPageContainer } from "./landing-page.style";
+
+const LandingPage = () => (
+    <LandingPageContainer>
+        LandingPage div
+    </LandingPageContainer>
+);
+
+export default LandingPage;
