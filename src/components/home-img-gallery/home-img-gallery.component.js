36fff7102cc98ed3abb37518abf17ac19c635523
landon-sturgeon <sturgeon.landon@gmail.com>
2020-01-08 20:11:49 -0800

    added all the images with their formatting

--- src/components/home-img-gallery/home-img-gallery.component.js
+++ src/components/home-img-gallery/home-img-gallery.component.js
@@ -1,10 +1,40 @@
 import React from "react";
 
-import { HomeImgGalleryContainer } from "./home-img-gallery.style";
+import Gal1 from "../../img/gal-1.jpeg";
+import Gal2 from "../../img/gal-2.jpeg";
+import Gal3 from "../../img/gal-3.jpeg";
+import Gal4 from "../../img/gal-4.jpeg";
+import Gal5 from "../../img/gal-5.jpeg";
+import Gal6 from "../../img/gal-6.jpeg";
+import Gal7 from "../../img/gal-7.jpeg";
+import Gal8 from "../../img/gal-8.jpeg";
+import Gal9 from "../../img/gal-9.jpeg";
+import Gal10 from "../../img/gal-10.jpeg";
+import Gal11 from "../../img/gal-11.jpeg";
+import Gal12 from "../../img/gal-12.jpeg";
+import Gal13 from "../../img/gal-13.jpeg";
+import Gal14 from "../../img/gal-14.jpeg";
+
+import { 
+    HomeImgGalleryContainer, GalleryImage 
+} from "./home-img-gallery.style";
 
 const HomeImgGallery = () => (
     <HomeImgGalleryContainer>
-        home img gallery content
+        <GalleryImage imgNum="one" src={Gal1} alt="Gallery image 1"/>
+        <GalleryImage imgNum="two" src={Gal2} alt="Gallery image 2"/>
+        <GalleryImage imgNum="three" src={Gal3} alt="Gallery image 3"/>
+        <GalleryImage imgNum="four" src={Gal4} alt="Gallery image 4"/>
+        <GalleryImage imgNum="five" src={Gal5} alt="Gallery image 5"/>
+        <GalleryImage imgNum="six" src={Gal6} alt="Gallery image 6"/>
+        <GalleryImage imgNum="seven" src={Gal7} alt="Gallery image 7"/>
+        <GalleryImage imgNum="eight" src={Gal8} alt="Gallery image 8"/>
+        <GalleryImage imgNum="nine" src={Gal9} alt="Gallery image 9"/>
+        <GalleryImage imgNum="ten" src={Gal10} alt="Gallery image 10"/>
+        <GalleryImage imgNum="eleven" src={Gal11} alt="Gallery image 11"/>
+        <GalleryImage imgNum="twelve" src={Gal12} alt="Gallery image 12"/>
+        <GalleryImage imgNum="thirteen" src={Gal13} alt="Gallery image 13"/>
+        <GalleryImage imgNum="fourteen" src={Gal14} alt="Gallery image 14"/>
     </HomeImgGalleryContainer>
 );
 
--- src/components/home-img-gallery/home-img-gallery.style.js
+++ src/components/home-img-gallery/home-img-gallery.style.js
@@ -1,8 +1,116 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import { COLORS } from "../../scss/_variables.style";
 
+const imgModifier = (props) => {
+    var outputCss = "";
+    switch (props.imgNum) {
+        case "one":
+            outputCss = css`
+                grid-row: 1 / span 2;
+                grid-column: 1 / span 2;
+            `;
+            break;
+        case "two":
+            outputCss = css`
+                grid-row: 1 / span 3;
+                grid-column: 3 / span 3;
+            `; 
+            break;
+        case "three":
+            outputCss = css`
+                grid-row: 1 / span 2;
+                grid-column: 6 / 7;
+            `; 
+            break;
+        case "four":
+            outputCss = css`
+                grid-row: 1 / span 2;
+                grid-column: 7 / span 2;
+            `; 
+            break;
+        case "five":
+            outputCss = css`
+                grid-row: 3 / span 3;
+                grid-column: 1 / span 2;
+            `; 
+            break;
+        case "six":
+            outputCss = css`
+                grid-row: 4 / span 2;
+                grid-column: 3 / span 2;
+            `; 
+            break;
+        case "seven":
+            outputCss = css`
+                grid-row: 4 / 5;
+                grid-column: 5 / 6;
+            `;
+            break; 
+        case "eight":
+            outputCss = css`
+                grid-row: 3 / span 2;
+                grid-column: 6 / span 2;
+            `; 
+            break;
+        case "nine":
+            outputCss = css`
+                grid-row: 3 / span 3;
+                grid-column: 8 / 9;
+            `; 
+            break;
+        case "ten":
+            outputCss = css`
+                grid-row: 6 / span 2;
+                grid-column: 1 / 2;
+            `;
+            break; 
+        case "eleven":
+            outputCss = css`
+                grid-row: 6 / span 2;
+                grid-column: 2 / span 2;
+            `;
+            break; 
+        case "twelve":
+            outputCss = css`
+                grid-row: 6 / span 2;
+                grid-column: 4 / 5;
+            `;
+            break; 
+        case "thirteen":
+            outputCss = css`
+                grid-row: 5 / span 3;
+                grid-column: 5 / span 3;
+            `;
+            break; 
+        case "fourteen":
+            outputCss = css`
+                grid-row: 6 / span 2;
+                grid-column: 8 / 9;
+            `;
+            break;
+        default:
+            outputCss = "";                                                                                               
+    }
+    return outputCss; 
+}
+
 export const HomeImgGalleryContainer = styled.div`
-    background-color: ${ COLORS.greyDark1 };
+    background-color: ${ COLORS.greyLight1 };
     grid-column: full-start / full-end;
+
+    display: grid;
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(7, 5vw);
+    grid-gap: 1.5rem;
+    padding: 1.5rem;
+`;
+
+export const GalleryImage = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    display: block;
+
+    ${ imgModifier }
 `;
