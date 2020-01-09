550f988a565aab5995938fbaea8967eedf1c9856
landon-sturgeon <sturgeon.landon@gmail.com>
2020-01-09 10:38:53 -0800

    added realtors to top of page

--- src/components/realtor-list/realtor-list.component.js
+++ src/components/realtor-list/realtor-list.component.js
@@ -1,10 +1,44 @@
 import React from "react";
 
-import { RealtorListContainer } from "./realtor-list.style";
+import realtor1 from "../../img/realtor-1.jpeg";
+import realtor2 from "../../img/realtor-2.jpeg";
+import realtor3 from "../../img/realtor-3.jpeg";
+
+import { Heading3 } from "../../scss/_typography.style";
+
+import { 
+    RealtorListContainer, Realtors, RealtorContainer,
+    RealtorImage, RealtorName, HomesSold, RealtorInfoBox
+} from "./realtor-list.style";
 
 const RealtorList = () => (
     <RealtorListContainer>
-        realtor list content
+        <Realtors>
+            <Heading3>top 3 realtors</Heading3>
+            <RealtorContainer>
+                <RealtorImage src={realtor1} alt="realtor1" />
+                <RealtorInfoBox>
+                    <RealtorName>Ted Mosby</RealtorName>
+                    <HomesSold>244 Home Sold</HomesSold>
+                </RealtorInfoBox>
+            </RealtorContainer>
+
+            <RealtorContainer>
+                <RealtorImage src={realtor2} alt="realtor2" />
+                <RealtorInfoBox>
+                    <RealtorName>Janet Janetson</RealtorName>
+                    <HomesSold>2,100 Home Sold</HomesSold>
+                </RealtorInfoBox>
+            </RealtorContainer>
+
+            <RealtorContainer>
+                <RealtorImage src={realtor3} alt="realtor3" />
+                <RealtorInfoBox>
+                    <RealtorName>Sir Arthur Peddlebottom</RealtorName>
+                    <HomesSold>44 Home Sold</HomesSold>
+                </RealtorInfoBox>
+            </RealtorContainer>
+        </Realtors>
     </RealtorListContainer>
 );
 
--- src/components/realtor-list/realtor-list.style.js
+++ src/components/realtor-list/realtor-list.style.js
@@ -5,4 +5,43 @@ import { COLORS } from "../../scss/_variables.style";
 export const RealtorListContainer = styled.div`
     background-color: ${ COLORS.secondary };
     grid-column: col-start 7 / full-end;
+
+    display: grid;
+    align-items: center;
+`;
+
+export const Realtors = styled.div`
+    display: grid;
+    justify-content: center;
+    grid-gap: 2rem;
+
+    & > h3 {
+        justify-self: center;
+    }
+`;
+
+export const RealtorContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, max-content);
+    align-items: center;
+`;
+
+export const RealtorImage = styled.img`
+    height: 6rem;
+    border-radius: 50%;
+    margin-right: 1.5rem;
+`;
+
+export const RealtorInfoBox = styled.div`
+    display: grid;
+    color: #fff;
+`;
+
+export const RealtorName = styled.span`
+    font-size: 1.6rem;
+    font-weight: 400;
+`;
+
+export const HomesSold = styled.span`
+    font-size: 1.4rem;
 `;
