3d64e1b5ecaef64a7f9bc48ef05d3493a1ba54cd
landon-sturgeon <sturgeon.landon@gmail.com>
2020-01-09 07:58:18 -0800

    added links and legal text to the footer

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -1,10 +1,33 @@
 import React from "react";
 
-import { FooterContainer } from "./footer.style";
+import { 
+    FooterContainer, FooterNavigation, NavItem, NavLink,
+    LegalText
+} from "./footer.style";
 
 const Footer = () => (
     <FooterContainer>
-        Footer content
+        <FooterNavigation>
+            <NavItem>
+                <NavLink href="#">Find your dream home</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="#">Request proposal</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="#">Download home planner</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="#">Contact us</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="#">Submit your property</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="#">Come work </NavLink>
+            </NavItem>
+        </FooterNavigation>
+        <LegalText>&copy; Copyright 2020 by Landon Sturgeon. Feel free to use this project template for yourself!</LegalText>
     </FooterContainer>
 );
 
--- src/components/footer/footer.style.js
+++ src/components/footer/footer.style.js
@@ -1,8 +1,50 @@
 import styled from "styled-components";
 
-import { COLORS } from "../../scss/_variables.style";
+import { COLORS, FONT } from "../../scss/_variables.style";
 
 export const FooterContainer = styled.div`
     background-color: ${ COLORS.secondary };
     grid-column: full-start / full-end;
+    padding: 8rem;
+`;
+
+export const FooterNavigation = styled.ul`
+    list-style: none;
+
+    display: grid;
+    grid-gap: 2rem;
+    grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+    align-items: center;
+`;
+
+export const NavItem = styled.li`
+    transition: all 0.2s;
+
+    &:hover {
+        transform: translateY(-0.7rem);
+    }
+`;
+
+export const NavLink = styled.a`
+    &:link,
+    &:visited {
+        font-size: 1.6rem;
+        color: #fff;
+        text-decoration: none;
+        font-family: ${FONT.display };
+        text-tansform: uppercase;
+        text-align: center;
+        padding: 1.5rem;
+        display: block;
+    }
+`;
+
+export const LegalText = styled.p`
+    font-size: 1.4rem;
+    color: ${ COLORS.greyLight2 };
+    margin-top: 6rem;
+    margin-right: auto;
+    margin-left: auto;
+    text-align: center;
+    width: 70%;
 `;
